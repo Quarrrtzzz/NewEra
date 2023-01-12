3025fcb84c9c538a8fb2adb52d06dbfa24a462d7
Quarrrtzzz <sid1.97@mail.ru>
2023-01-12 17:58:24 +0300

    Movies

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,18 @@
-let number = 5;
-const leftBorderWidth = 1;
-console.log('arr' + "- object");
-console.log(4 + "5");
+const numberOfFilms = prompt('Сколько фильмов вы смотрели?', "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+    },
+    actors: {
+    },
+    genres: [],
+    privat: false
+} ;
+
+const a = prompt('Один из последних просмотренных фильмов?', ""),
+      b = prompt('На сколько оцените его?', ""),
+      c = prompt('Один из последних просмотренных фильмов?', ""),
+      d = prompt('На сколько оцените его?', "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
